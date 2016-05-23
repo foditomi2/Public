1ff4a684a67e7b9062ce0457ba029b763f42fbc9
Mikecz Márk László <mikecz.mark.laszlo@gmail.com>
2016-05-23 09:12:30 +0200

    Update

--- Taller/Second.cpp
+++
@@ -1,5 +0,0 @@
-#include <iostream>
-
-std::ostream& gvar = std::cout << "Tallér és ";
-
-int main() {}
--- Taller/Third.cpp
+++
@@ -1,5 +0,0 @@
-#include <iostream>
-
-std::ostream& teszt = std::cout << "Bia" << std::endl;
-
-int main2() {}
